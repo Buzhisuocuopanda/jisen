eb32d504ec931821afa9805a98adc9202d9bc320
浩源 刘 <15698178812@163.com>
2022-10-25 10:42:24 +0800

    1

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/impl/SalesreturnordersServiceImpl.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/impl/SalesreturnordersServiceImpl.java
@@ -121,7 +121,7 @@ public class SalesreturnordersServiceImpl implements ISalesreturnordersService {
                 throw new SwException("销售退库数量不能为空");
             }
             if(Objects.isNull(itemList.get(i).getCbsf11())){
-                throw new SwException("销售退库数量不能为空");
+                throw new SwException("销售退库价格不能为空");
             }
             if(Objects.isNull(itemList.get(i).getCbse01())){
                 throw new SwException("销售退库id不能为空");
@@ -751,6 +751,18 @@ if(cbsgss.size()>0){
                 throw new SwException("该商品不在销售出库单明细中");
             }*/
 //            cbsf.setCbsf01(good.getCbsf01());
+
+
+            if(Objects.isNull(good.getCbsf08())){
+                throw new SwException("销售退库商品不能为空");
+            }
+            if(Objects.isNull(good.getCbsf09())){
+                throw new SwException("销售退库数量不能为空");
+            }
+            if(Objects.isNull(good.getCbsf11())){
+                throw new SwException("销售退库价格不能为空");
+            }
+
             cbsf.setCbse01(cbseDo.getCbse01());
             cbsf.setCbsf02(good.getCbsf02());
             cbsf.setCbsf03(date);
